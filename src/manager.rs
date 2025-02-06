8042c07fea0c3b6c4b2c7397fac074f65ce2f8ce
Lalit Kumar Bhasin <labhas@microsoft.com>
2025-02-06 01:45:19 +0000

    fix

--- src/manager.rs
+++ src/manager.rs
@@ -9,7 +9,7 @@ pub struct Manager {
     probes: Vec<Arc<Probe>>,
     controller: Arc<Mutex<Controller>>,
 }
-use tokio::task::spawn_local;
+use tokio::task;
 
 impl Manager {
     pub fn new(controller: Arc<Mutex<Controller>>) -> Result<Self> {
@@ -26,7 +26,7 @@ impl Manager {
     pub async fn run(&self) -> Result<()> {
         for probe in &self.probes {
             let probe = Arc::clone(probe);
-            spawn_local(async move {
+            task::spawn(async move {
                 let probe_guard = probe.bpf_object.lock().await; // ✅ Lock bpf_object inside async block
 
                 if let Err(e) = probe.run().await {
